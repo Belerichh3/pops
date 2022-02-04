e38dd0d887e5f45ed40d04b188c2cc813908d782
Glitch (hello-express) <none>
2022-02-04 01:20:17 +0000

    🌕💆 Checkpoint ./ardademr.js:25915949/614

--- ardademr.js
+++ ardademr.js
@@ -74,7 +74,15 @@ member.roles.add(role);
 // ArdaDemr Youtube Kanalına ait KAYIT bot altyapısı
 
 client.on("ready", () => {
-  client.channels.get("856144839988871208").join();
-})
+       const channel = client.channels.cache.get('938965651886854185');
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+        // Yay, it worked!
+        console.log("Successfully connected.");
+    }).catch(e => {
 
+        // Oh no, it errored! Let's log it to console :)
+        console.error(e);
+    });
+});
 
